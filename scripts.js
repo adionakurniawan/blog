9c1a0db6a5c8e971d945c4da342f07b5cd15b8f0
Adi Kurniawan <kurniawan.uchiha01@gmail.com>
2018-09-03 11:10:41 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 (function() {
   'use strict';
 
+  // Menu Toggle Function
   var menuToggle = document.querySelector('.menu-toggle');
   var body = document.querySelector('body');
 
@@ -9,4 +10,21 @@
   }
 
   menuToggle.addEventListener('click', toggleMenu);
+
+  // Unwrap p tag from img element
+  var images = document.querySelectorAll('p img');
+  images.forEach(function(img) {
+    img.parentElement.setAttribute('class', 'img');
+  });
+
+  // Modify youtube link to responsive embed element
+  var youtubeLinks = document.querySelectorAll('a[href*="youtube.com/watch"]');
+
+  youtubeLinks.forEach(function(link) {
+    link.parentElement.setAttribute('class', 'youtube');
+    link.parentElement.innerHTML =
+      '<iframe src="https://www.youtube.com/embed/' +
+      link.href.split('v=')[1].split('&')[0] +
+      '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+  });
 })();
